47638c7b83e60871c20a56548e98fa0d97246aab
= <hopekr@dongguk.edu>
2020-11-02 05:16:45 +0900

    modifed sql file

--- openeg_sql.sql
+++ openeg_sql.sql
@@ -16,6 +16,7 @@ CREATE TABLE `project_tb` (
   `user_id` varchar(100) NOT NULL,
   `project_name` varchar(100) NOT NULL,
   `project_type` varchar(100) NOT NULL,
+  `project_path` varchar(100) NOT NULL,
   `project_created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`project_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -24,12 +25,6 @@ CREATE TABLE `project_tb` (
 --
 -- Dumping data for table `project_tb`
 --
-
-LOCK TABLES `project_tb` WRITE;
-/*!40000 ALTER TABLE `project_tb` DISABLE KEYS */;
-INSERT INTO `project_tb` VALUES ('4c572a95-7508-4a53-8509-879c35dafbf8','3674f6b0-8e9f-432b-9f80-64286d4883c2','Project Test Nodejs','Nodejs','2020-10-05 02:29:57'),('61fe4aa4-4f3d-4375-9877-b263be73c1dd','3674f6b0-8e9f-432b-9f80-64286d4883c2','Project Spring ','Java Spring','2020-10-05 02:31:09'),('ecdeb22e-8a06-441e-ab9c-88227e0b8043','3674f6b0-8e9f-432b-9f80-64286d4883c2','Project Test Python','Python','2020-10-05 02:30:22');
-/*!40000 ALTER TABLE `project_tb` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `project_tb_spring`
@@ -55,8 +50,3 @@ CREATE TABLE `project_tb_spring` (
 -- Dumping data for table `project_tb_spring`
 --
 
-LOCK TABLES `project_tb_spring` WRITE;
-/*!40000 ALTER TABLE `project_tb_spring` DISABLE KEYS */;
-INSERT INTO `project_tb_spring` VALUES ('5b384e41-b31a-4053-b5c2-0d04b12ffb37','61fe4aa4-4f3d-4375-9877-b263be73c1dd','Maven','Java','2.4.0','group','des','package name');
-/*!40000 ALTER TABLE `project_tb_spring` ENABLE KEYS */;
-UNLOCK TABLES;
